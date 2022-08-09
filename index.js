b66d6d8c1683055365158b8d0e6a122bc8567a32
Max Thorson <max@mst.mn>
2022-08-09 13:27:24 -0500

    fix movie clean function

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import 'dotenv/config'
 
 import cleanMovies from './lib/movies/index.js'
 import cleanTV from './lib/tv/index.js'
+import { resyncOmbiMedia } from './lib/apis/ombi.js'
 
 async function run() {
  await cleanMovies()
- await cleanTV()   
+ await cleanTV()
+ 
+ // Media resync applies to both movies and TV.
+ await resyncOmbiMedia()
 }
 
 run()
